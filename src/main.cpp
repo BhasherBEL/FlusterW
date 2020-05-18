788f866ef0c673d3a2ff1a8e63372357ba10324f
Bhasher <bhasher.anonyme@gmail.com>
2020-05-18 21:55:00 +0200

    2020.05.18.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,51 @@
 #include <iostream>
 #include <array>
 #include <chrono>
+#include <windows.h>
+#pragma comment(lib, "Crypt32")
 
 #include "components/chrome.h"
-#include "utils/logging.h"
+#include "utils/db.h"
+#include "../contrib/base64.h"
+
+std::string decryptToken(const std::string& cipher_text) {
+    DATA_BLOB input;
+    input.pbData = const_cast<BYTE*>(reinterpret_cast<const BYTE*>(cipher_text.data()));
+    input.cbData = static_cast<DWORD>(cipher_text.length());
+    DATA_BLOB output;
+    BOOL result = ::CryptUnprotectData(&input, nullptr, nullptr, nullptr, nullptr,
+                                       CRYPTPROTECT_UI_FORBIDDEN, &output);
+
+    if (!result){
+        std::cerr << "error: " << result << std::endl;
+        return std::string();
+    }
+
+    std::string refresh_token(reinterpret_cast<char*>(output.pbData),
+                              output.cbData);
+    ::LocalFree(output.pbData);
+    return refresh_token;
+}
 
 int main(){
     srand(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+    std::cout << std::boolalpha;
 
-    std::cout << std::boolalpha << Chrome::run(std::filesystem::temp_directory_path().string() + "\\browsers")  << std::endl;
+    //std::cout << Chrome::run(std::filesystem::temp_directory_path().string() + "\\browsers")  << std::endl;
 
-    /*Log log{"main.log", true, true};
+    std::string const userDataPath{std::string(std::getenv("LOCALAPPDATA")) + "\\Google\\Chrome\\User Data\\"};
 
-    for(ChromiumBasedBrowser const &browser : Chrome::presentBrowsers()){
-        std::string const path{browser.getPath()};
-        log << "> " << browser.name << log.endl();
-        log << "\t|--> Path: " << path << log.endl();
-        log << "\t|--> Version: " << Chrome::getVersion(path)[0] << log.endl();
-        log << "\t|--> Profiles:" << log.endl();
+    std::string encryptedTokenb64{Chrome::getEncryptionKey(userDataPath)};
 
-        auto profiles = Chrome::getProfiles(path);
-        for (Profile const &profile : profiles){
-            log << "\t\t|--> " << profile.name << log.endl();
-            if(!profile.customFullName.empty() && profile.customFullName != "None")
-                log << "\t\t\t|--> Full Name: " << profile.customFullName << log.endl();
-            if(!profile.id.empty() && profile.id != "None")
-                log << "\t\t\t|--> ID: " << profile.id << log.endl();
-            if(!profile.customShortcutName.empty() && profile.customShortcutName !="None")
-                log << "\t\t\t|--> Pseudo: " << profile.customShortcutName << log.endl();
-            if(!profile.email.empty() && profile.email != "None")
-                log << "\t\t\t|--> Email: " << profile.email << log.endl();
-            if(!profile.pictureUrl.empty() && profile.pictureUrl != "None")
-                log << "\t\t\t|--> Picture url: " << profile.pictureUrl << log.endl();
-            if(profile.lastUsing != -1 && profile.lastUsing != 0)
-                log << "\t\t\t|--> Last using: " << (int)profile.lastUsing << log.endl();
+    std::cout << "'" << encryptedTokenb64 << "'" << std::endl << std::endl;
 
-            auto const history{Chrome::getHistory(profile.path.string())};
-            log << "\t\t\t|--> History: " << history.size() << " entries" << log.endl();
-        }
-    }*/
+    std::string encryptedToken{base64_decode(encryptedTokenb64)};
+
+    std::cout << "'" << encryptedToken << "'" << std::endl << std::endl;
+
+    std::string decryptedToken{decryptToken(encryptedToken)};
+
+    std::cout << "'" << decryptedToken << "'" << std::endl << std::endl;
 
     return 0;
 }
